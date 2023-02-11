0d709bf76ce047a308458570acae07a88582eb95
Edward Sandor <public+git@sandorlaboratories.com>
2023-02-11 10:15:43 -0700

    Organize source files

--- inc/seismometer_types.hpp
+++ inc/seismometer_types.hpp
@@ -1,7 +1,10 @@
 #ifndef __SEISMOMETER_TYPES_HPP__
 #define __SEISMOMETER_TYPES_HPP__
 
-/*Temperature in milli-Celsius*/
+/* Temperature in milli-Celsius */
 typedef int m_celsius_t;
+
+/* Acceleration in millimeters per second^2*/
+typedef int mm_ps2_t;
 
 #endif /*__SEISMOMETER_TYPES_HPP__*/
