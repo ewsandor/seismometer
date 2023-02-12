e9dc61f8747bd9ae4777be8fb9789e0ea0513e1a
Edward Sandor <public+git@sandorlaboratories.com>
2023-02-12 15:57:09 -0700

    Parse data sample string to python dictionary

---
+++ monitor/parser.py
@@ -0,0 +1,17 @@
+def twos_complement(hexstr, bits):
+    value = int(hexstr, 16)
+    if value & (1 << (bits - 1)):
+        value -= 1 << bits
+    return value
+
+def parse_seismometer_line(line):
+  line_split = line.split('|')
+  if((5 == len(line_split)) and ('SAMPLE' == line_split[0])):
+    sample = {
+      'key'      : int(line_split[1],16),
+      'index'    : int(line_split[2],16),
+      'timestamp': int(line_split[3],16),
+      'data'     : twos_complement(line_split[4],64),
+    }
+    print(line)
+    print(sample)
--- monitor/seismometer_monitor.py
+++ monitor/seismometer_monitor.py
@@ -1,13 +1,61 @@
 #!/bin/python3
 
+import getopt
+import serial
 import sys
 
+from parser import parse_seismometer_line
+
+program_name_str="Sandor Laboratories Seismometer Monitor"
+version_str="0.0.1-dev"
+publish_year_str="February 2023"
+author_str="Edward Sandor"
+website_str="https://git.sandorlaboratories.com/edward/seismometer"
+
+title_block_str=program_name_str +"\n" \
+                "Version " + version_str + "\n" \
+                + author_str + ", " + publish_year_str + "\n" \
+                + website_str
+
 def main(argv) -> int:
-    print("Sandor Laboratories Seismometer Monitor")
-    print("Version 0.0.1-dev")
-    print("Edward Sandor, February 2023")
-    print("https://git.sandorlaboratories.com/edward/seismometer")
-    return 0
+  print(title_block_str)
+
+  #Init working variables
+  default_serial_path="/dev/ttyACM0"
+  serial_path=default_serial_path
+  default_serial_baud=115200
+  serial_baud=default_serial_baud
+
+  help_string=title_block_str + "\n\n" \
+              "This script monitors, analyzes, and visualizes data from the seismometer data collector.\n\n" \
+              "Arguments:\n" \
+              "   -h             --help             Prints this Help information and exits.\n" \
+              "   -b <baudrate>, --baud <baudrate>  Serial device baud.  Defaults to '" + str(default_serial_baud) + "'\n" \
+              "   -s <path>,     --serial=<path>    Serial device path.  Defaults to '" + default_serial_path + "'\n" \
+
+  #Parse command line arguments
+  try:
+      opts, args = getopt.getopt(argv,"hs:",["help", "serial=",])
+  except getopt.GetoptError as err:
+      print(err)
+      print("\n"+help_string)
+      sys.exit(22)
+
+  for opt, arg in opts:
+      if opt in ('-b', "--baud"): 
+          serial_baud = int(baud)
+      elif opt in ('-h', "--help"): 
+          print(help_string)
+          sys.exit()
+      elif opt in ('-s', "--serial"): 
+          serial_path = arg
+
+  with serial.Serial(serial_path, serial_baud, timeout=1) as ser:
+    while 1:
+      line = ser.readline().decode('utf-8').strip()
+      parse_seismometer_line(line)
+
+  return 0
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+  sys.exit(main(sys.argv[1:]))
