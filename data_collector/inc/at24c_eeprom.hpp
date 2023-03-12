4c8d75b25848ccfa849d4093a8902ad2993ed469
Edward Sandor <public+git@sandorlaboratories.com>
2023-03-11 17:24:12 -0700

    AT24C EEPROM outlining

---
+++ data_collector/inc/at24c_eeprom.hpp
@@ -0,0 +1,52 @@
+#ifndef __AT24C_EEPROM_HPP__
+#define __AT24C_EEPROM_HPP__
+
+#include <hardware/i2c.h>
+
+typedef enum
+{
+  AT24C_EEPROM_ADDRESS_0,
+  AT24C_EEPROM_ADDRESS_1,
+  AT24C_EEPROM_ADDRESS_2,
+  AT24C_EEPROM_ADDRESS_3,
+  AT24C_EEPROM_ADDRESS_4,
+  AT24C_EEPROM_ADDRESS_5,
+  AT24C_EEPROM_ADDRESS_6,
+  AT24C_EEPROM_ADDRESS_7,
+  AT24C_EEPROM_ADDRESS_MAX,
+} at24c_eeprom_address_e;
+
+typedef enum
+{
+  AT24C_EEPROM_SIZE_32K,
+  AT24C_EEPROM_SIZE_64K,
+  AT24C_EEPROM_SIZE_MAX,
+} at24c_eeprom_size_e;
+
+typedef unsigned int at24c_eeprom_data_size_t;
+typedef unsigned int at24c_eeprom_data_address_t;
+
+class at24c_eeprom_c
+{
+  private:
+    i2c_inst_t                   *i2c_inst;
+    const at24c_eeprom_address_e  address;
+    const uint8_t                 i2c_addr;
+    const at24c_eeprom_size_e     size;
+  public:
+    at24c_eeprom_c(i2c_inst_t *, at24c_eeprom_address_e, at24c_eeprom_size_e);
+
+    inline at24c_eeprom_size_e      get_size()       const { return size; };
+    inline at24c_eeprom_data_size_t get_size_bytes() const { return ((AT24C_EEPROM_SIZE_64K==size)?4096:8192); };
+
+    /* Reads 'bytes_to_read' bytes from the eeprom into 'buffer' starting at the given 'start_address'. 
+        'buffer' must be large enough to hold 'bytes_to_read' bytes.
+        Returns number of bytes read */
+    at24c_eeprom_data_size_t read_data(at24c_eeprom_data_address_t start_address, const uint8_t * buffer, at24c_eeprom_data_size_t bytes_to_read);
+    /* Writes 'bytes_to_write' bytes to the eeprom from 'buffer' starting at the given 'start_address'. 
+        'buffer' must hold at least 'bytes_to_read' bytes.
+        Returns number of bytes written */
+    at24c_eeprom_data_size_t write_data(at24c_eeprom_data_address_t start_address, const uint8_t * buffer, at24c_eeprom_data_size_t bytes_to_write);
+};
+
+#endif /* __AT24C_EEPROM_HPP__ */
---
+++ data_collector/src/at24c_eeprom.cpp
@@ -0,0 +1,54 @@
+#include <cstring>
+
+#include "at24c_eeprom.hpp"
+#include "seismometer_debug.hpp"
+#include "seismometer_utils.hpp"
+
+#define AT4C_EEPROM_BASE_ADDRESS 0xA0
+
+at24c_eeprom_c::at24c_eeprom_c(i2c_inst_t * i2c_init, at24c_eeprom_address_e address_init, at24c_eeprom_size_e size_init)
+  : address(address_init), size(size_init), i2c_addr(AT4C_EEPROM_BASE_ADDRESS | (address_init & 0x7))
+{
+  SEISMOMETER_ASSERT((address >= 0) && (address < AT24C_EEPROM_ADDRESS_MAX));
+  SEISMOMETER_ASSERT((size >= 0)    && (size    < AT24C_EEPROM_SIZE_MAX));
+  SEISMOMETER_ASSERT(i2c_init != nullptr);
+  i2c_inst = i2c_init;
+}
+
+
+at24c_eeprom_data_size_t at24c_eeprom_c::read_data(at24c_eeprom_data_address_t start_address, const uint8_t * buffer, at24c_eeprom_data_size_t bytes_to_read)
+{
+  at24c_eeprom_data_size_t ret_val = 0;
+  SEISMOMETER_ASSERT(buffer != nullptr);
+
+  if((bytes_to_read > 0) && (start_address < get_size_bytes()))
+  {
+    ret_val = SEISMOMETER_MIN(bytes_to_read, (get_size_bytes()-start_address));
+  }
+
+  return ret_val;
+}
+at24c_eeprom_data_size_t at24c_eeprom_c::write_data(at24c_eeprom_data_address_t start_address, const uint8_t * buffer, at24c_eeprom_data_size_t bytes_to_write)
+{
+  at24c_eeprom_data_size_t bytes_written = 0;
+  SEISMOMETER_ASSERT(buffer != nullptr);
+
+  if((bytes_to_write > 0) && (start_address < get_size_bytes()))
+  {
+    at24c_eeprom_data_size_t btw = SEISMOMETER_MIN(bytes_to_write, (get_size_bytes()-start_address));
+    uint8_t write_buffer[34]; /* 32-byte page + 2 byte address*/
+    write_buffer[0] = (start_address>>8) & ((AT24C_EEPROM_SIZE_64K==size)?0x1F:0x0F);
+    write_buffer[1] = (start_address&0xFF);
+
+    while(btw > 0)
+    {
+      at24c_eeprom_data_size_t btw_now = ((btw > 32)?32:btw);
+      memcpy(&write_buffer[1], &buffer[bytes_written], btw_now);
+      SEISMOMETER_ASSERT_CALL((btw_now+2) == i2c_write_blocking(i2c_inst, i2c_addr, write_buffer, btw_now+2, true));
+      bytes_written+=btw_now;
+      btw -= btw_now;
+    }
+  }
+
+  return bytes_written;
+}
